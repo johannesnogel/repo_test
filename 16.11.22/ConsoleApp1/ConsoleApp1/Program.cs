d69c61bc163f586eda0f3e72dd668516241cb926
johannesnogel <118337193+johannesnogel@users.noreply.github.com>
2022-11-16 11:20:56 +0200

    Update Program.cs

--- 16.11.22/ConsoleApp1/ConsoleApp1/Program.cs
+++ 16.11.22/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,23 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your favorite football player t-shirt number");
+            string number = Console.ReadLine();
+            int numberParsed = Int16.Parse(number);
+
+            // Teen siia if ja else loogikaga vastused
+            // nt kui on alla 50, siis tuleb console writeline vastus
+
+            if (numberParsed <= 20)
+
+                Console.WriteLine("Enter your favorite football players name");
+            string name = Console.ReadLine();
+            int nameParsed = Int16.Parse(name);
+
+            // 
+
+                       
+            
         }
     }
 }
